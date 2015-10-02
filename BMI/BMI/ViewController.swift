bfe383689a902378ad2aab4d878ad4873e0df10a
Peter Paul <peterpaul1999@gmx.de>
2015-10-02 15:17:41 +0200

    Complete BMI

--- BMI/BMI/ViewController.swift
+++ BMI/BMI/ViewController.swift
@@ -18,13 +18,42 @@ class ViewController: UIViewController {
     @IBOutlet var outputLabel: UILabel!
     
     @IBAction func calculateTapped(sender: AnyObject) {
+        
+        let kgValue = Int(kgField.text!)
+        let cmValue = Int(cmField.text!)
+        
+        var output: String?
+        
+        if kgValue != nil && cmValue != nil {
+            let sizeInMeter = Double(cmValue!) / 100
+            let bmi = Int(Double(kgValue!) / (sizeInMeter * sizeInMeter))
+            
+            output = "Dein BMI: \(bmi)"
+        } else {
+            output = "CM oder KG nicht lesbar"
+        }
+        
+        if output != nil {
+            outputLabel.hidden = false
+            outputLabel.text = output
+        }
     }
     
     @IBAction func kgSet(sender: AnyObject) {
+        kgField.text = "\(Int(kgSider.value))"
     }
     
+    @IBAction func reset(sender: AnyObject) {
+        kgField.text = "70"
+        cmField.text = "170"
+        kgSider.value = 70
+        cmSlider.value = 170
+        outputLabel.text = "Ergebnis"
+        outputLabel.hidden = true
+    }
     
     @IBAction func cmSet(sender: AnyObject) {
+        cmField.text = "\(Int(cmSlider.value))"
     }
     
     override func viewDidLoad() {
